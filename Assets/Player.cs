8b17ec8bdb7d6f089861adf9d65f969f52c40181
LENOVO-PC\Bjurtis <kaspar-pawlak@hotmail.com>
2018-03-21 13:41:30 +0100

    message

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float jumpSpee = 20;
+    public float gravity = 9.8f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
